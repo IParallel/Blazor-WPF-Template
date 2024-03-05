4c30e2d53cb6291136841eaaa3dc9890f1a08fe6
Bello <bello.dev99@gmail.com>
2024-03-05 04:28:36 -0300

    added

---
+++ BlazorWPF.App/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows;
+using BlazorWPF.WebApp;
+using Microsoft.AspNetCore.Components.WebView.Wpf;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BlazorWPF.App;
+
+/// <summary>
+///     Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        var webView = new BlazorWebView
+        {
+            HostPage = @"wwwroot\index.html"
+        };
+        var services = new ServiceCollection();
+        services.AddWpfBlazorWebView();
+#if DEBUG
+        services.AddBlazorWebViewDeveloperTools();
+#endif
+        webView.Services = services.BuildServiceProvider();
+        webView.RootComponents.Add(new RootComponent
+        {
+            ComponentType = typeof(Root),
+            Selector = "#app"
+        });
+        Content = webView;
+
+        AppDomain.CurrentDomain.UnhandledException += (sender, error) =>
+        {
+            MessageBox.Show(error.ExceptionObject.ToString(), "Unhandled Exception", MessageBoxButton.OK,
+                MessageBoxImage.Error);
+        };
+    }
+}
